f1b016a0b5c4856bf50545d73ef44938de8bb6fb
rahul11nishad <rahul004700@gmail.com>
2021-12-25 05:03:16 +0530

    second commit

--- funPrograming.js
+++ funPrograming.js
@@ -16,6 +16,8 @@ Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
+
+// 1111111111111111111111111111111111111111111111111111test1111111111111111
 const getTea = (numOfCups) => {
   const teaCups = [];
 
@@ -25,3 +27,36 @@ const getTea = (numOfCups) => {
   }
   return teaCups;
 };
+let preaper=getTea(40);
+
+// 22222222222222test22222
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea,27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea,13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
